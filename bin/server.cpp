7a933f337edb041170788c7e32c9c0b9f5b83198
ABE-Mark45 <ahmedinmasr@gmail.com>
2023-05-17 06:00:13 +0200

    Added logging to server

--- bin/server.cpp
+++ bin/server.cpp
@@ -2,19 +2,44 @@
 #include <thrift/server/TSimpleServer.h>
 #include <thrift/transport/TBufferTransports.h>
 #include <thrift/transport/TServerSocket.h>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include "dispatcher/Dispatcher.h"
 #include "gen-cpp/GraphService.h"
 #include "graph/graph.h"
+#include "logger/Logger.h"
 
 class GraphServiceHandler : public graph_rpc::GraphServiceIf {
  private:
   graph::Graph graph_;
   dispatcher::Dispatcher dispatcher_;
+  logger::Logger logger_;
+
+  void initGraph(const std::string& path) {
+    std::ifstream inputStream(path);
+
+    std::string line;
+
+    while (std::getline(inputStream, line) && line != "S") {
+      std::stringstream ss(line);
+      graph::NodeIdType u, v;
+      ss >> u >> v;
+      graph_.addEdge(u, v);
+    }
+    inputStream.close();
+  }
 
  public:
-  GraphServiceHandler(coordinator::CoordinatorType type)
-      : dispatcher_(graph_, type) {}
+  GraphServiceHandler(coordinator::CoordinatorType type,
+                      const std::string& inputGraphPath,
+                      const std::string& outputLogFile)
+      : dispatcher_(graph_, type), logger_(outputLogFile) {
+    logger_.println("Starting Server");
+    logger_.println("Initializing graph");
+    initGraph(inputGraphPath);
+    logger_.println("Graph initialized successfully");
+  }
 
   void sendQueryBatch(
       std::vector<graph::DistanceType>& result,
@@ -23,10 +48,29 @@ class GraphServiceHandler : public graph_rpc::GraphServiceIf {
   }
 };
 
-int main() {
+int main(int argc, char** argv) {
+  if (argc != 4) {
+    throw std::runtime_error(
+        "./server <bfs | cached_bfs | all_pairs> <input-graph> <output-log>");
+  }
+
+  std::string type(argv[1]);
+  coordinator::CoordinatorType coordinatorType;
+  if (type == "bfs") {
+    coordinatorType = coordinator::BFS;
+  } else if (type == "cached_bfs") {
+    coordinatorType = coordinator::CACHED_BFS;
+  } else if (type == "all_pairs") {
+    coordinatorType = coordinator::ALL_PAIRS;
+  } else {
+    throw std::runtime_error(
+        "./server <bfs | cached_bfs | all_pairs> <input-graph> <output-log>");
+  }
+
   apache::thrift::server::TSimpleServer server(
       std::make_shared<graph_rpc::GraphServiceProcessor>(
-          std::make_shared<GraphServiceHandler>(coordinator::CACHED_BFS)),
+          std::make_shared<GraphServiceHandler>(coordinatorType, argv[2],
+                                                argv[3])),
       std::make_shared<apache::thrift::transport::TServerSocket>(9090),
       std::make_shared<apache::thrift::transport::TBufferedTransportFactory>(),
       std::make_shared<apache::thrift::protocol::TBinaryProtocolFactory>());
